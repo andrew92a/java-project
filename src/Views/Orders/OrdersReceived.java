82766f631af794cce8fdedb16f41eed186b72882
Pawel Glowacz <pawel199307@gmail.com>
2016-04-27 23:02:19 +0200

    Nie wyswietla mi forma po kliknieciu na zlecenia

---
+++ src/Views/Orders/OrdersReceived.java
@@ -0,0 +1,11 @@
+package Views.Orders;
+
+import javax.swing.*;
+
+/**
+ * Created by pawel on 4/27/16.
+ */
+public class OrdersReceived {
+    private JTextArea zleceniaTextArea;
+    private JPanel panel1;
+}
